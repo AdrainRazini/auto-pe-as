b4d844f37319333f043183952d882ed0eae1aee2
Adrian Razini Rangel <160901383+AdrainRazini@users.noreply.github.com>
2025-06-05 14:34:59 +0000

    Comit 1

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const path = require('path');
 
+const app = express();
+const PORT = 3000;
+
+// Definir a pasta pública para servir arquivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rota para servir o index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+// isso aqui roda na url :) em uma porta kkk
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
